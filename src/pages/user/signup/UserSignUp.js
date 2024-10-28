fc07e1e1a470e6e557bf80af128b9fa00c762581
JoaoBenignodev <joao.benigno@aluno.faculdadeimpacta.com.br>
2024-10-28 02:13:57 -0300

    Implementing MaterialUI to the User's Signup form

--- src/pages/user/signup/UserSignUp.js
+++ src/pages/user/signup/UserSignUp.js
@@ -1,23 +1,24 @@
 // Importing
 import { useState } from 'react';
-import './style.css'
+import { Card, CardContent, Typography, TextField, Button } from '@mui/material';
+import './style.css';
 
 function UserSignUp() {
 
     // useState hook to manage form input data
     // Initial state of the formData is set with empty strings for each field
-    const [formData, setFormData] = useState ({
+    const [formData, setFormData] = useState({
         name: '',
         email: '',
         password: '',
-        document:'',
+        document: '',
         is_active: true // Default value
     })
 
     // Function for handling form input changes
     const handleChange = (e) => {
         const { name, value } = e.target;
-        setFormData(defaultState => ({...defaultState,[name]: value}));
+        setFormData(defaultState => ({ ...defaultState, [name]: value }));
     };
 
     // Function for submitting the creation of a User
@@ -30,86 +31,84 @@ function UserSignUp() {
 
         try {
             const response = await fetch('http://localhost:8080/api/users/add/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        })
-
-        const data = await response.json();
-
-        if (response.ok) {
-            console.log('User created with success!', data);
-            alert('Your user was created with success!')
-
-            // Resetting the form data
-            setFormData({
-                name: '',
-                email: '',
-                password: '',
-                document:'',
-                is_active: true // Default value
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
             })
-        } else {
-            console.error('Failed to sign up the User:', data);
-            alert('Failed to Sign Up!\nPlease try again!')
-        }}
-        catch(error) {
+
+            const data = await response.json();
+
+            if (response.ok) {
+                console.log('User created with success!', data);
+                alert('Your user was created with success!')
+
+                // Resetting the form data
+                setFormData({
+                    name: '',
+                    email: '',
+                    password: '',
+                    document: '',
+                    is_active: true // Default value
+                })
+            } else {
+                console.error('Failed to sign up the User:', data);
+                alert('Failed to Sign Up!\nPlease try again!')
+            }
+        }
+        catch (error) {
             console.error('Error Signing Up:', error)
             alert('An error occured while signing up!')
-        };
+        }
     }
 
-    return(
-        <div id='user-signup-form'>
-            <h2>Sign Up</h2>
-            <form>
-                <label form='name'>Name</label>
-                <input
-                    type='text'
-                    name='name'
-                    id='user-signup-name-input'
-                    value={formData.name}
-                    onChange={handleChange}
-                />
-                
-                <label form='email'>E-mail</label>
-                <input
-                    type='email'
-                    name='email'
-                    id='user-signup-email-input'
-                    value={formData.email}
-                    onChange={handleChange}
-                />
-                
-                <label form='password'>Password</label>
-                <input
-                    type='password'
-                    name='password'
-                    id='user-signup-pswd-input'
-                    value={formData.password}
-                    onChange={handleChange}
-                />
-                
-                <label form='document'>Document</label>
-                <input
-                    type='text'
-                    name='document'
-                    id='user-signup-document-input'
-                    value={formData.document}
-                    onChange={handleChange}
-                />
-                
-                {/*Including "is_active" field in the data sent*/}
-                <input type='hidden' name='is_active' value={formData.is_active}/>
+    // Array of field configurations
+    const fields = [
+        { label: 'Nome', name: 'name', type: 'text' },
+        { label: 'E-mail', name: 'email', type: 'email' },
+        { label: 'Password', name: 'password', type: 'password' },
+        { label: 'Document', name: 'document', type: 'text' }
+    ];
 
-                <input
-                    type='button' 
-                    value='Sign-up'
-                    onClick={submitUser}
-                />
-            </form>
+    return (
+        <div id='user-signup-form'>
+            <Card sx={{ width: 400 }}>
+                <CardContent>
+                    <Typography variant='h4' component='h2' gutterBottom>
+                        Cadastre-se
+                    </Typography>
+                    <form>
+                        {fields.map((field) => (
+                            <TextField
+                                key={field.name}
+                                label={field.label}
+                                variant='outlined'
+                                name={field.name}
+                                type={field.type}
+                                margin='normal'
+                                fullWidth
+                                value={formData[field.name]}
+                                onChange={handleChange}
+                                required
+                            />
+                        ))}
+
+                        {/*inclding " is_active" field in the data sent*/}
+                        <input type='hidden' name='is_active' value={formData.is_active} />
+
+                        <Button
+                            variant='contained'
+                            size='large'
+                            fullWidth
+                            sx={{ backgroundColor: '#3949ab', marginTop: 1}}
+                            onClick={submitUser}
+                        >
+                            Cadastrar
+                        </Button>
+                    </form>
+                </CardContent>
+            </Card>
         </div>
     )
 }
