1ad688ab4048ecdd0fcbbe19278696f5b1ba548e
JoaoBenignodev <joao.benigno@aluno.faculdadeimpacta.com.br>
2024-10-28 02:50:54 -0300

    Implementing MaterialUI's Snackbar and Alert to validation messages

--- src/pages/user/signup/UserSignUp.js
+++ src/pages/user/signup/UserSignUp.js
@@ -1,9 +1,14 @@
 // Importing
 import { useState } from 'react';
-import { Card, CardContent, Typography, TextField, Button } from '@mui/material';
+import { Card, CardContent, Typography, TextField, Button, Snackbar, Alert } from '@mui/material';
 import './style.css';
 
 function UserSignUp() {
+
+    // UseState hook for managing alerts
+    const [openAlert, setOpenAlert] = useState(false) // Controls the visibility of the alert
+    const [alertMessage, setAlertMessage] = useState('') // Holds the message to be displayed in the alert
+    const [alertSeverity, setAlertSeverity] = useState('') // Defines the "type" of the alert's severity
 
     // useState hook to manage form input data
     // Initial state of the formData is set with empty strings for each field
@@ -25,7 +30,9 @@ function UserSignUp() {
     async function submitUser() {
         // Check if all required fileds are filled
         if (!formData.name || !formData.email || !formData.password || !formData.document) {
-            alert('To proceed with the Sign-up, all the required fileds must be filled out!');
+            setAlertMessage('To proceed with the Sign-up, all the required fileds must be filled out!');
+            setAlertSeverity('warning');
+            setOpenAlert(true);
             return;
         }
 
@@ -42,7 +49,9 @@ function UserSignUp() {
 
             if (response.ok) {
                 console.log('User created with success!', data);
-                alert('Your user was created with success!')
+                setAlertMessage('Your user was created with success!')
+                setAlertSeverity('success');
+                setOpenAlert(true);
 
                 // Resetting the form data
                 setFormData({
@@ -54,12 +63,16 @@ function UserSignUp() {
                 })
             } else {
                 console.error('Failed to sign up the User:', data);
-                alert('Failed to Sign Up!\nPlease try again!')
+                setAlertMessage('Failed to Sign Up!\nPlease try again!')
+                setAlertSeverity('error');
+                setOpenAlert(true);
             }
         }
         catch (error) {
             console.error('Error Signing Up:', error)
-            alert('An error occured while signing up!')
+            setAlertMessage('An error occured while signing up!')
+            setAlertSeverity('error');
+            setOpenAlert(true);
         }
     }
 
@@ -94,14 +107,14 @@ function UserSignUp() {
                             />
                         ))}
 
-                        {/*inclding " is_active" field in the data sent*/}
+                        {/* Including " is_active" field on the formData */}
                         <input type='hidden' name='is_active' value={formData.is_active} />
 
                         <Button
                             variant='contained'
                             size='large'
                             fullWidth
-                            sx={{ backgroundColor: '#3949ab', marginTop: 1}}
+                            sx={{ backgroundColor: '#3949ab', marginTop: 1 }}
                             onClick={submitUser}
                         >
                             Cadastrar
@@ -109,6 +122,22 @@ function UserSignUp() {
                     </form>
                 </CardContent>
             </Card>
+
+            {/* Snackbar for alerts*/}
+            <Snackbar
+                open={openAlert}
+                autoHideDuration={6000} // Duration on milliseconds before closing the alert
+                onClose={() => setOpenAlert(false)}
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }} // Snackbar's screen relative position
+            >
+                <Alert
+                    onClose={() => setOpenAlert(false)}
+                    severity={alertSeverity}
+                    sx={{ width: '100%' }}
+                >
+                    {alertMessage}
+                </Alert>
+            </Snackbar>
         </div>
     )
 }
