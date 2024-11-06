26b7739943f28d3b5c14b2f6501cd4c30c40c8f5
DerliJunior <contact.derli@gmail.com>
2024-11-06 20:04:06 -0300

    feat: :art: add Product page to router

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import UserSignUp from './pages/user/signup/UserSignUp';
-import UserListAll from './pages/users/listAll/UserListAll';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import UserSignUp from "./pages/user/signup/UserSignUp";
+import Products from "./pages/product/products";
+import UserListAll from "./pages/users/listAll/UserListAll";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App/>}/>
-        <Route path='/user-signup/' element={<UserSignUp/>}/>
-        <Route path='/user-list-all/' element={<UserListAll/>}/>
+        <Route path="/" element={<App />} />
+        <Route path="/user-signup/" element={<UserSignUp />} />
+        <Route path="/products/" element={<Products />} />
+        <Route path="/user-list-all/" element={<UserListAll />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
