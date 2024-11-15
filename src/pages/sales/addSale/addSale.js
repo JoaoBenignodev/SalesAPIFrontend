f1a3c552c481a6a1fd7b53ec443fcb05e8e23654
marimontefusco <mari.montefusco@gmail.com>
2024-11-15 14:08:37 -0300

    fixing bugs at Sale registration page

--- src/pages/sales/addSale/addSale.js
+++ src/pages/sales/addSale/addSale.js
@@ -1,228 +1,204 @@
-import { useState, useEffect } from 'react';
-import { Box, Button, Container, Divider, MenuItem, Paper, TextField } from "@mui/material";
-import { Grid } from '@mui/joy';
+import { useState, useEffect, useCallback } from 'react';
+import { Card, CardContent, Typography, TextField, Button, FormControl, InputLabel, Select, MenuItem, Alert, Snackbar } from '@mui/material'
+import './style.css';
 
 function AddSale() {
 
-  const [users, setUsers] = useState([]);
-  const [products, setProducts] = useState([]);
-  const [saleProducts, setSaleProducts] = useState(
-    [
-      {
-        product_id: '',
+    const [openAlert, setOpenAlert] = useState(false) // Controls the visibility of the alert
+    const [alertMessage, setAlertMessage] = useState('') // Holds the message to be displayed in the alert
+    const [alertSeverity, setAlertSeverity] = useState('') // Defines the "type" of the alert's severity
+
+    // UseState hook to manage User data
+    const [users, setUsers] = useState([]);
+    const [products, setProducts] = useState([]);
+
+    // Function to fecth all the Users
+    const fetchUsers = useCallback(() => {
+        fetch('http://localhost:8080/api/users/')
+            .then((response) => response.json())
+            .then((data) => {
+                setUsers(data);
+            })
+            .catch((error) => {
+                console.error('Error: ', error);
+            });
+    }, []);
+
+    // Function to fecth all the Products
+    const fetchProducts = useCallback(() => {
+        fetch('http://localhost:8080/api/products/')
+            .then((response) => response.json())
+            .then((data) => {
+                setProducts(data);
+            })
+            .catch((error) => {
+                console.error('Error: ', error);
+            });
+    }, []);
+
+    // useEffect hook to synchronize all the data queried from the fetchUsers
+    useEffect(() => {
+        fetchUsers();
+    }, [fetchUsers]);
+
+    // useEffect hook to synchronize all the data queried from the fetchProducts
+    useEffect(() => {
+        fetchProducts();
+    }, [fetchProducts]);
+
+    // useState hook to manage form input data
+    // Initial state of the formData is set with empty strings for each field
+    const [formData, setFormData] = useState({
         quantity: '',
-        // price: '',
-      }
-    ]
-  );
-  const [saleData, setSaleData] = useState({
-    user_id: '',
-    products: saleProducts,
-  });
-  // const [openAlert, setOpenAlert] = useState(false);
-  // const [alertMessage, setAlertMessage] = useState('');
-  // const [alertSeverity, setAlertSeverity] = useState('');
+        user_id: '', // Parent(User) key
+        product_id: '', // Parent(Product) key
+    });
 
-  // Function to fetch users and products
-  useEffect(() => {
-    fetchUsers();
-    fetchProducts();
-  }, []);
-
-  // Function to fetch users
-  const fetchUsers = async () => {
-    try {
-      const response = await fetch('http://localhost:8080/api/users/');
-      const data = await response.json();
-      setUsers(data);
-        
-    } catch(error) {
-      console.error("Erro ao carregar clientes: ", error);
-    }
-  };
-
-  // Function to fetch products
-  const fetchProducts = async () => {
-    try {
-      const response = await fetch('http://localhost:8080/api/products/');
-      const data = await response.json();
-      setProducts(data);
-
-    } catch (error) {
-      console.error("Erro ao carregar produtos: ", error);
-    }
-  };
-
-  // Function to update selected Customer
-  const handleUserChange = (e) => {
-    setSaleData((prevSaleData) => ({
-      ...prevSaleData,
-      user_id: e.target.value,
-    }));
-  };
-
-  // Function to update selected product data
-  const handleProductChange = (index, field, value) => {
-    const updatedSaleProducts = [...saleProducts];
-    updatedSaleProducts[index][field] = value;
-
-    if (field === 'product_id') {
-      const selectedProduct = products.find((product) => product.id === value);
-      updatedSaleProducts[index].product = selectedProduct ? selectedProduct.name : '';
-      // updatedSaleProducts[index].price = selectedProduct ? selectedProduct.price : '';
-    }
-
-    setSaleProducts(updatedSaleProducts);
-    setSaleData((prevSaleData) => ({
-      ...prevSaleData,
-      products: updatedSaleProducts,
-    }))
-
-  };
-
-  // Function to add a new product to the form
-  const addProductField = () => {
-    setSaleProducts([...saleProducts, {
-      product_id: '', 
-      product: '', 
-      quantity: '', 
-      // price: '' 
-    }]);
-  };
-
-  // Function to submit form
-  const handleSubmit = async (e) =>  {
-    e.preventDefault();
-
-    const saleDto = {
-      user_id: saleData.user_id,
-      products: saleProducts.map((item) => ({
-        product_id: item.product_id,
-        product: item.product,
-        quantity: item.quantity,
-        // price: item.price,
-      })),
+    // Function for handling form input changes
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(defaultState => ({...defaultState, [name]: value}));
     };
 
-    try {
-      const response = await fetch('http://localhost:8080/api/sales/add/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(saleDto)
-      });
+    // Function for submitting the creation of a Product
+    async function submitSale() {
 
-      if (response.ok) {
-        alert('Venda cadastrada com sucesso!');
-        setSaleData({
-          user_id: '',
-          products: [
-            {
-              product_id: '', 
-              product: '',
-              quantity: '',
-              // price: '',
-            },
-          ]
-        });
+        // Check if all the required fields are filled
+        if (!formData.quantity || !formData.user_id || !formData.product_id) {
+            setAlertMessage('To proceed with the creation, all the required fileds must be filled out!');
+            setAlertSeverity('warning');
+            setOpenAlert(true);
+            return
+        }
 
-      } else {
-        alert('Erro ao cadastrar a venda.');
-        console.log('Erro na resposta: ', response);
-      }
+        // Check if the given Quantity is bigger than 0
+        if (formData.quantity <= 0) {
+            setAlertMessage('A sale needs to have at least 1 unit!');
+            setAlertSeverity('warning');
+            setOpenAlert(true);
+            return;
+        } 
 
-    } catch (error) {
-      console.log("Erro ao cadastrar a venda: ", error);
+        try {
+            const response = await fetch('http://localhost:8080/api/sales/add/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                console.log('Sale created with success!', data);
+                setAlertMessage('The sale was created with success!');
+                setAlertSeverity('success');
+                setOpenAlert(true);
+
+                // Reseting the form data
+                setFormData({
+                    quantity: '',
+                    user_id: '',
+                    product_id: '',
+                })
+            } else {
+                console.error('Failed to create Sale!', data);
+                setAlertMessage('Failed to register Sale!\nPlease try again!');
+                setAlertSeverity('error');
+                setOpenAlert(true);
+            }
+        } 
+        catch (error) {
+            console.log('And error occured while creating the Sale!', error);
+            setAlertMessage('An error occured while registering the Sale!');
+            setAlertSeverity('error');
+            setOpenAlert(true);
+        }
     }
-  };
 
-  return(
-    <Box backgroundColor="#e8eaf6" p ={8} display="flex" justifyContent="center" sx={{minHeight: '100vh'}}>
-      <Container component={Paper}>
-        <form onSubmit={handleSubmit}>
-          <h1 align="center">Cadastrar Venda</h1>
-          <Divider/>
+    return (
+        <div id='sale-creation-form'>
+            <Card elevation={10} sx={{ width: 400 }}>
+                <CardContent>
+                    <Typography variant='h4' component='h2' gutterBottom>
+                        Cadastrar Nova Venda
+                    </Typography>
+                    <form>
+                        {/* Quantity field configuration */}
+                        <TextField
+                        label="Quantidade"
+                        variant="outlined"
+                        name="quantity"
+                        type="number"
+                        fullWidth
+                        value={formData.quantity}
+                        onChange={handleChange}
+                        required
+                        />
 
-          <h4>Id do Cliente</h4>
-          <TextField
-            select
-            label="Id do Cliente"
-            name="user_id"
-            value={saleData.user_id} ////////ou {selectedUser} sem o .id
-            onChange={handleUserChange}
-            fullWidth
-            margin="normal"
-            required
-          >
-            { users.map((user) => (
-              <MenuItem key={user.id} value={user.id}>
-                {user.name}
-              </MenuItem>
-            ))}
-          </TextField>
-          
-          {saleProducts.map((product, index) => (
-            <Grid container spacing={2} key={index} alignItems="center">
+                        {/* UserId field configuration */}
+                        <FormControl variant='outlined' fullWidth margin="normal">
+                            <InputLabel>Cliente</InputLabel>
+                            <Select
+                            label="Cliente"
+                            name="user_id"
+                            value={formData.user_id}
+                            onChange={handleChange}
+                            required
+                            >
+                                {users.map((user) => (
+                                    <MenuItem key={user.id} value={user.id}>
+                                        {user.name}
+                                    </MenuItem>
+                                ))};
+                            </Select>
+                        </FormControl>
 
-              <Grid item xs={3}>
-                <h4>Id do Produto</h4>
-                <TextField
-                  select
-                  label="Produto"
-                  value={product.product_id}
-                  onChange={(e) => handleProductChange(index, 'product_id', e.target.value)}
-                  fullWidth
-                  required
-                >
-                  {products.map((prod) => (
-                    <MenuItem key={prod.id} value={prod.id}>
-                      {prod.name}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              </Grid>
-              
-              {/* <Grid item xs={3}>
-                <h4>Id do Cliente</h4>
-                <TextField
-                  label="Preço Unitário"
-                  value={product.price}
-                  fullWidth
-                  // InputProps={{
-                  //   readOnly: true,
-                  // }}
-                />
-              </Grid> */}
+                        {/* ProductId field configuration */}
+                        <FormControl variant="outlined" fullWidth margin="normal">
+                            <InputLabel>Produto</InputLabel>
+                            <Select 
+                            label="Produto"
+                            name="product_id"
+                            value={formData.product_id}
+                            onChange={handleChange}
+                            required>
+                            
+                                {products.map((product) => (
+                                    <MenuItem key={product.id} value={product.id}>
+                                        {product.name}
+                                    </MenuItem>
+                                ))};
+                            </Select>
+                        </FormControl>
 
-              <Grid items xs={3}>
-                <h4>Id do Cliente</h4>
-                <TextField
-                  type="number"
-                  label="Quantidade"
-                  value={product.quantity}
-                  onChange={(e) => handleProductChange(index, 'quantity', e.target.value)}
-                  fullWidth
-                  required
-                  // inputProps={{ min: 1}}
-                />
-              </Grid>
-            </Grid>
-          ))}
+                        <Button
+                        variant="contained"
+                        size="large"
+                        fullWidthsx={{backgroundColor: '#3949ab', marginTop: 1 }}
+                        onClick={submitSale}>
+                            Cadastrar
+                        </Button>
+                    </form>
+                </CardContent>
+            </Card>
 
-          <Button onClick={addProductField} color="primary">
-            Adicionar mais produtos
-          </Button>
-
-          <Button type="submit" variant="contained" color="primary" fullWidth>
-            Cadastrar Venda
-          </Button>
-
-        </form>
-      </Container>
-    </Box>
-
-  );
-}
+            {/* Snackbar for returning on-screen alerts */}
+            <Snackbar
+            open={openAlert}
+            autoHideDuration={6000}
+            onClose={() => setOpenAlert(false)}
+            anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
+                <Alert
+                onClose={() => setOpenAlert(false)}
+                severity={alertSeverity}
+                sx={{ width: '100%'}}>
+                    {alertMessage}
+                </Alert>
+            </Snackbar>
+        </div>
+    );
+};
 
 export default AddSale;
