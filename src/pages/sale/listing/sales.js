9a52e0effa0b1443cafe182de3289d135f2b135e
JoaoBenignodev <joao.benigno@aluno.faculdadeimpacta.com.br>
2024-11-26 01:42:19 -0300

    Updating Sales listing to only print Sales

--- src/pages/sale/listing/sales.js
+++ src/pages/sale/listing/sales.js
@@ -1,25 +1,14 @@
-import { useCallback, useEffect, useState } from 'react';
-import { Alert, Box, Button, Divider, IconButton, Modal, Paper, Snackbar, Table, TableContainer, TableHead, TableRow, TableCell, TableBody, TextField, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import { useEffect, useState } from 'react';
+import { Alert, Box, Button, Divider, Paper, Snackbar, Table, TableContainer, TableHead, TableRow, TableCell, TableBody } from "@mui/material";
 import { formatLocale } from '../../../utils/formatLocale';
 import EditIcon from '@mui/icons-material/Edit';
 import AddIcon from '@mui/icons-material/Add';
-
-
 
 function Sales() {
 
     // useState hook to store the list of Sales
     const [sales, setSales] = useState([]);
 
-    // useState hook to manage Users data
-    const [users, setUsers] = useState([]);
-
-    // useState hook to manage Products data
-    const [products, setProducts] = useState([]);
-
-    const [currentSale, setCurrentSale] = useState(null);
-    const [formValues, setFormValues] = useState({ quantity: '', price: '', user_id: '', product_id: '' });
-    const [openModal, setOpenModal] = useState(false);
     const [openSnackbar, setOpenSnackbar] = useState(false);
     const [snackbarMessage, setSnackbarMessage] = useState('');
     const [snackbarSeverity, setSnackbarSeverity] = useState('success');
@@ -40,15 +29,15 @@ function Sales() {
             if (response.ok) {
                 setSales(data); //Setting the fetched sales data
             } else {
-                console.error('Failed to fetch sales', data);
-                setSnackbarMessage('Erro ao buscar as vendas');
+                console.error('Failed to fetch Sales!', data);
+                setSnackbarMessage('An error occured while fetching the sales!');
                 setSnackbarSeverity('error');
                 setOpenSnackbar(true);
             }
 
         } catch (error) {
             console.log('Error fetching sales:', error);
-            setSnackbarMessage('Erro ao buscar as vendas');
+            setSnackbarMessage('An error occured!');
             setSnackbarSeverity('error');
             setOpenSnackbar(true);
         }
@@ -58,123 +47,6 @@ function Sales() {
     useEffect(() => {
         fetchSales();
     }, []);
-
-
-    // Function to fecth all the Users for the customer.id selection listinig on the Update Modal
-    const fetchUsers = useCallback(() => {
-        fetch('http://localhost:8080/api/users/')
-            .then((response) => response.json())
-            .then((data) => {
-                setUsers(data);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }, []);
-
-    // useEffect hook to synchronize all the data queried from the fetchUsers
-    useEffect(() => {
-        fetchUsers();
-    }, [fetchUsers]);
-
-    // Function to fecth all the Users for the customer.id selection listinig on the Update Modal
-    const fetchProducts = useCallback(() => {
-        fetch('http://localhost:8080/api/products/')
-            .then((response) => response.json())
-            .then((data) => {
-                setProducts(data);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }, []);
-
-    // useEffect hook to synchronize all the data queried from the fetchProducts
-    useEffect(() => {
-        fetchProducts();
-    }, [fetchProducts]);
-
-
-    // Function to handle Sale update
-    function updateSale(sale) {
-        setCurrentSale(sale); //set the current sale to be edited
-        setFormValues({
-            quantity: sale.quantity,
-            price: sale.price,
-            user_id: sale.user_id,
-            product_id: sale.product_id,
-        }); //populate form with sales data
-        setOpenModal(true);
-    }
-
-    // Function to handle form input changes
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setFormValues({ ...formValues, [name]: value });
-    }
-
-    // Function to submit the updated sale data
-    async function handleFormSubmit() {
-        // Check if all the required fields are filled
-        if (!formValues.quantity || !formValues.user_id || !formValues.product_id) {
-            setSnackbarMessage('To proceed with the update, all the required fileds must be filled out!');
-            setSnackbarSeverity('warning');
-            setOpenSnackbar(true);
-            return
-        }
-
-        // Check if the given Quantity or Price are greater than 0
-        if (formValues.quantity <= 0) {
-            setSnackbarMessage('A Sale needs to have at least 1 Product unit!');
-            setSnackbarSeverity('warning');
-            setOpenSnackbar(true);
-            return;
-        }
-
-        try {
-            const response = await fetch(`http://localhost:8080/api/sales/${currentSale.id}/change/`,
-                {
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(formValues),
-                });
-
-            const data = response.json()
-
-            if (response.ok) {
-                console.log("Sale updated with success!", data);
-                setSnackbarMessage('The Sale was updated with success!');
-                setSnackbarSeverity("success");
-                setOpenSnackbar(true);
-
-                setOpenModal(false); // Closes the modal
-
-                // Fetch the Product listing considering the updated data
-                fetchSales()
-
-                // Fetch the User to ensure newly registered Users are listed in the FormControl 
-                fetchUsers()
-
-                // Fetch the Product to ensure newly registered Products are listed in the FormControl 
-                fetchProducts()
-
-
-            } else {
-                console.error("Failed to update the Sale!", data);
-                setSnackbarMessage("Failed to update the Sale!\nPlease try again!");
-                setSnackbarSeverity("error");
-                setOpenSnackbar(true);
-            }
-
-        } catch (error) {
-            console.error('An error occured while updating the Sale!', error);
-            setSnackbarMessage("An error occured while updating the Sale!");
-            setSnackbarSeverity("error");
-            setOpenSnackbar(true);
-        }
-    };
 
     // Close snackbar function
     const handleCloseSnackbar = () => {
@@ -200,15 +72,33 @@ function Sales() {
                     >
                         Sales
                     </h1>
-                    <Button
-                        variant='contained'
-                        size='large'
-                        href="/sales/add/"
-                        endIcon={<AddIcon />}
-                        sx={{ fontWeight: 'bold', backgroundColor: '#3949ab' }}
+                    <Box
+                        paddingRight={1}
                     >
-                        ADD
-                    </Button>
+                        <Button
+                            variant='contained'
+                            size='large'
+                            href="/sales/active/"
+                            endIcon={<EditIcon />}
+                            sx={{ fontWeight: 'bold', backgroundColor: '#3949ab' }}
+                        >
+                            EDIT
+                        </Button>
+                    </Box>
+                    <Box
+                        paddingLeft={1}
+                    >
+                        <Button
+                            paddingX={2}
+                            variant='contained'
+                            size='large'
+                            href="/sales/add/"
+                            endIcon={<AddIcon />}
+                            sx={{ fontWeight: 'bold', backgroundColor: '#3949ab' }}
+                        >
+                            ADD
+                        </Button>
+                    </Box>
                 </Box>
                 <Divider />
                 <Table arial-label="sale table">
@@ -240,8 +130,9 @@ function Sales() {
                             </TableCell>
                             <TableCell
                                 sx={{ fontWeight: ' bold' }}
-                                align="center">
-                                Actions
+                                align="center"
+                            >
+                                Status
                             </TableCell>
                         </TableRow>
                     </TableHead>
@@ -259,78 +150,43 @@ function Sales() {
                                 <TableCell align="center">{sale.user_name}</TableCell>
                                 <TableCell align="center">{sale.quantity}</TableCell>
                                 <TableCell align="center">{formatLocale(sale.price)}</TableCell>
-                                <TableCell align="center">
-                                    <IconButton aria-label="edit sale" onClick={() => updateSale(sale)}>
-                                        <EditIcon />
-                                    </IconButton>
+                                <TableCell
+                                    align="center"
+                                    style={{ alignContent: "left" }}
+                                >
+                                    <Box
+                                        display="flex"
+                                        justifyContent="center"
+                                        width="full"
+                                    >
+                                        {sale.is_active ? (
+                                            <Box
+                                                sx={{ minWidth: 60, fontWeight: 'bold' }}
+                                                px={2}
+                                                bgcolor="#7986cb"
+                                                borderRadius="10px"
+                                                color="#FFF"
+                                            >
+                                                Active
+                                            </Box>
+                                        ) : (
+                                            <Box
+                                                sx={{ minWidth: 60, fontWeight: 'bold' }}
+                                                px={2}
+                                                bgcolor="#ff7878cb"
+                                                borderRadius="10px"
+                                                color="#FFF"
+                                            >
+                                                Inactive
+                                            </Box>
+                                        )}
+                                    </Box>
                                 </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
                 </Table>
             </TableContainer>
-
-            {/* Modal Component for editing sale*/}
-            <Modal open={openModal}
-                onClose={() => setOpenModal(false)}>
-
-                <Box backgroundColor="#FFF" sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 350, p: 4, borderRadius: '8px' }}>
-
-                    <h2 align="center">Edit Sale</h2>
-
-                    {/* UserId field configuration */}
-                    <FormControl variant="outlined" fullWidth margin="normal">
-                        <InputLabel>Customer</InputLabel>
-                        <Select
-                            label="Customer"
-                            name="user_id"
-                            value={formValues.user_id}
-                            onChange={handleInputChange}
-                            required
-                        >
-                            {users.map((user) => (
-                                <MenuItem key={user.id} value={user.id}>
-                                    {user.name}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-
-                    {/* ProductId field configuration */}
-                    <FormControl variant="outlined" fullWidth margin="normal">
-                        <InputLabel>Product</InputLabel>
-                        <Select
-                            label="Product"
-                            name="product_id"
-                            value={formValues.product_id}
-                            onChange={handleInputChange}
-                            required
-                        >
-                            {products.map((product) => (
-                                <MenuItem key={product.id} value={product.id}>
-                                    {product.name}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-
-
-                    {/* Quantity field configuration */}
-                    <TextField
-                        label="Quantity"
-                        name="quantity"
-                        value={formValues.quantity}
-                        onChange={handleInputChange}
-                        fullWidth
-                        margin="normal"
-                    />
-                    <Box display="flex" justifyContent="center" mt={2}>
-                        <Button variant="contained" size="large" onClick={handleFormSubmit} sx={{ backgroundColor: "#3949ab" }}>
-                            Update Sale
-                        </Button>
-                    </Box>
-                </Box>
-            </Modal>
 
             {/* Snackbar Component */}
             <Snackbar open={openSnackbar} autoHideDuration={6000} onClose={handleCloseSnackbar}>
